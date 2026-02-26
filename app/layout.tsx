62a8fe5e2398ceaa20120ecce25abbeabbab5ec9
Rijan Dahal <reasondahal719@gmail.com>
2026-02-26 12:55:34 +0545

    Fix(?): i dont even know anymore

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AvatarProvider } from "./hooks/useAvatar";
+import { Suspense } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#0b1622]`}
       >
-        <AvatarProvider>{children}</AvatarProvider>
+        <Suspense fallback={null}>
+          <AvatarProvider>{children}</AvatarProvider>
+        </Suspense>
       </body>
     </html>
   );
