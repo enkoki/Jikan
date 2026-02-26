a94fbca1745c4f12c379b851943e471975dc116a
Rijan Dahal <reasondahal719@gmail.com>
2026-02-26 12:52:58 +0545

    Fix: Build fail due to suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,23 @@
 "use client"
-import Image from "next/image";
+import React, { Suspense } from "react"; // 1. Import Suspense
 import Navbar from "./components/Navbar/Navbar";
 import Landing from "./components/Landing/Landing";
 import Trending from "./components/Anime/Trending";
 import AllTimePopular from "./components/Anime/AllTimePopular";
 import Upcoming from "./components/Anime/Upcoming";
+import Spinner from "./assets/icons/Spinner"; // Use your spinner for the fallback
 
 export default function Home() {
-	return(
-		<>
-			<Navbar />
-			<Landing />
-			<Trending />
+    return(
+        <>
+            <Navbar />
+            <Landing />
+            {/* 2. Wrap everything that uses Search/useSearchParams in Suspense */}
+            <Suspense fallback={<div className="flex justify-center p-10"><Spinner /></div>}>
+                <Trending />
+            </Suspense>
 			<Upcoming />
 			<AllTimePopular />
-		</>
-	);
+        </>
+    );
 }
